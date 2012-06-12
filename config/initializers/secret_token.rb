c2f73dff01b1dd8a11df9ac6c567ba31427c8430
Nick Miceli <nmiceli@kingkullen.com>
2012-06-12 09:48:42 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlassPumps::Application.config.secret_token = '2a068e3ffd387f0b337ef064e4643df75f83ec35ca4c4a6bccc536f06fe8a458104bbffb1998fdf0f1224250e42cb7bf0444846d27405b921fbfa787d992d828'
